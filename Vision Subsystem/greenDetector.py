98d511df356a3d1d68e564280c485e25b52d22e4
vedeti123 <vedeti123@gmail.com>
2024-10-24 18:35:15 -0500

    Added brown filter to detect pixels that are brown and definitely marks a divot

--- Vision Subsystem/greenDetector.py
+++ Vision Subsystem/greenDetector.py
@@ -12,54 +12,93 @@ from skimage.color import rgb2gray
 
 def greenDetector():
     rawImg = cv2.imread("pictures\Divot1.jpg")
-    divotDetectorGrayBin()
+    # divotDetectorGrayBin()
 
-    # plt.imshow(rawImg)
-    # plt.show()
+    plt.imshow(rawImg)
+    plt.show()
     # cv2.imshow("Raw Image", rawImg)
     # cv2.waitKey(0)
-    # treshImg = gradient_thresh(rawImg)
-    # plt.imshow(treshImg)
-    # plt.show()
+    treshImg = gradient_thresh(rawImg)
+    plt.imshow(treshImg)
+    plt.show()
     # cv2.imshow("Tresh Image", treshImg)
     # cv2.waitKey(0)
 
-    def color_thresh(self, img, thresh=(100, 255)):
-        """
-        Convert RGB to HSL and threshold to binary image using S channel
-        """
-        # 1. Convert the image from RGB to HSL
-        # 2. Apply threshold on S channel to get binary image
-        # Hint: threshold on H to remove green grass
+    start_time = time.perf_counter()
+    maskedImg = color_thresh(rawImg)
+    end_time = time.perf_counter()
+    # with np.set_printoptions(threshold=np.inf):
+    #     print(maskedImg[2000])
+    print("--- %s seconds ---" % (end_time - start_time))
+    plt.imshow(maskedImg)
+    plt.show()
 
-        # 1 - Convert image from RGB to HSL
-        hsl_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+def color_thresh(img):
+    """
+    Convert RGB to HSL and threshold to binary image using S channel
+    """
+    # 1. Convert the image from RGB to HSL
+    # 2. Apply threshold on S channel to get binary image
+    # Hint: threshold on H to remove green grass
+    blur = cv2.GaussianBlur(img, (5, 5), cv2.BORDER_DEFAULT)
+    # 1 - Convert image from RGB to HSL
+    hsl_img = cv2.cvtColor(blur, cv2.COLOR_BGR2HLS)
 
-        # 2 - Threshold L channel for white-looking lines
-        white_filter = cv2.inRange(hsl_img[:, :, 1], 215, 255)
+    # 2 - Threshold L channel for white-looking lines
+    # white_filter = cv2.inRange(hsl_img[:, :, 1], 215, 255)
 
-        if ROSBAG_3:
-            yellow_low = np.array([25, 0, 90])
-            yellow_high = np.array([35, 255, 255])
-            white_filter1 = cv2.inRange(hsl_img[:, :, 1], 150, 255)
-            white_filter = white_filter1
+    # Brown Filter Iteration 1
+    '''
+    brown_filter = cv2.inRange(hsl_img[:, :, 0], 20, 40)  # hue threshold for yellow-brown
+    brown_filter2 = cv2.inRange(hsl_img[:, :, 0], 40, 60)  # hue threshold for reddish-brown
 
-            yellow_filter = cv2.inRange(hsl_img, yellow_low, yellow_high)
-            yw_mask = cv2.bitwise_or(yellow_filter, white_filter) / 255  # Convert 0-255 to 0-1
+    # combine the two thresholds using bitwise OR
+    brown_filter = cv2.bitwise_or(brown_filter, brown_filter2)
 
-            dilate_element = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-            yw_mask = cv2.dilate(yw_mask, dilate_element)
-            return yw_mask
+    # apply saturation threshold to filter out non-brown colors
+    brown_filter = cv2.bitwise_and(brown_filter, cv2.inRange(hsl_img[:, :, 1], 20, 60))  # saturation threshold
+    brown_filter = cv2.bitwise_and(brown_filter, cv2.inRange(hsl_img[:, :, 2], 20, 50))  # lightness threshold
+    '''
 
-        # 3 - Dilate area a little to coincide with sobel output later down the pipeline
-        yw_mask = white_filter / 255  # Convert 0-255 to 0-1
+    #Brown Filter Iteration 2
 
-        dilate_element = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-        yw_mask = cv2.dilate(yw_mask, dilate_element)
+    # hue threshold with a range of +/- 10
+    brown_filter_hue = cv2.inRange(hsl_img[:, :, 0], 46, 66)
 
-        ####
+    # saturation threshold with a range of +/- 10%
+    brown_filter_sat = cv2.inRange(hsl_img[:, :, 1], 78, 98)
 
-        return yw_mask
+    # lightness threshold with a range of +/- 5%
+    brown_filter_light = cv2.inRange(hsl_img[:, :, 2], 5, 15)
+
+    # combine the three thresholds using bitwise AND
+    brown_filter = cv2.bitwise_and(brown_filter_hue, brown_filter_sat)
+    brown_filter = cv2.bitwise_and(brown_filter, brown_filter_light)
+
+
+
+    # if ROSBAG_3:
+    #     yellow_low = np.array([25, 0, 90])
+    #     yellow_high = np.array([35, 255, 255])
+    #     white_filter1 = cv2.inRange(hsl_img[:, :, 1], 150, 255)
+    #     white_filter = white_filter1
+    #
+    #     yellow_filter = cv2.inRange(hsl_img, yellow_low, yellow_high)
+    #     yw_mask = cv2.bitwise_or(yellow_filter, white_filter) / 255  # Convert 0-255 to 0-1
+    #
+    #     dilate_element = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    #     yw_mask = cv2.dilate(yw_mask, dilate_element)
+    #     return yw_mask
+
+    # 3 - Dilate area a little to coincide with sobel output later down the pipeline
+    yw_mask = brown_filter / 255  # Convert 0-255 to 0-1
+
+    dilate_element = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    yw_mask = cv2.dilate(yw_mask, dilate_element)
+
+    ####
+
+    return yw_mask
 
 
 def divotDetectorGrayBin():
