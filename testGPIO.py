49149873485e0b2dcf61fc09749c52c6c2a2e354
Ved Eti <vedeti123@gmail.com>
2024-12-02 16:07:41 -0600

    GPIO Test

--- testGPIO.py
+++ testGPIO.py
@@ -1,30 +1,55 @@
-import gpiod
+# import gpiod
+# import time
+
+# chip = gpiod.Chip('gpiochip4')
+
+
+# divotPin = 20
+# greenPin = 21
+# #Initialize your pin
+# #GPIO.setup(divotDet,GPIO.OUT)
+# #GPIO.setup(greenDet, GPIO.OUT)ls 
+
+# divotGPIO = chip.get_line(divotPin)
+# greenGPIO = chip.get_line(greenPin)
+
+# divotGPIO.request(consumer = 'my_gpio', type = gpiod.LINE_REQ_DIR_OUT)
+# greenGPIO.request(consumer = 'my_gpio', type = gpiod.LINE_REQ_DIR_OUT)
+
+
+# divotGPIO.set_value(1)
+
+# boole = True
+
+# while (1):
+#     if boole:
+#         greenGPIO.set_value(1)
+#     else:
+#         greenGPIO.set_value(0)
+#     boole = not boole
+#     time.sleep(2)
+
+
+
+import gpiozero
 import time
 
-chip = gpiod.Chip('gpiochip4')
 
+if __name__=="__main__":
+    divotPin = 20
+    greenPin = 21
 
-divotPin = 20
-greenPin = 21
-#Initialize your pin
-#GPIO.setup(divotDet,GPIO.OUT)
-#GPIO.setup(greenDet, GPIO.OUT)ls 
+    divotGPIO = gpiozero.DigitalOutputDevice(divotPin, initial_value=False)
+    greenGPIO = gpiozero.DigitalOutputDevice(greenPin, initial_value=False)
 
-divotGPIO = chip.get_line(divotPin)
-greenGPIO = chip.get_line(greenPin)
+    divotGPIO.on()
+    greenGPIO.on()
 
-divotGPIO.request(consumer = 'my_gpio', type = gpiod.LINE_REQ_DIR_OUT)
-greenGPIO.request(consumer = 'my_gpio', type = gpiod.LINE_REQ_DIR_OUT)
+    GPIO16 = gpiozero.DigitalOutputDevice(16, initial_value=False)
+    GPIO16.on()
 
+    print("Hello")
+    while 1:
+        pass
 
-divotGPIO.set_value(1)
-
-boole = True
-
-while (1):
-    if boole:
-        greenGPIO.set_value(1)
-    else:
-        greenGPIO.set_value(0)
-    boole = not boole
-    time.sleep(2)
+    print("d")
